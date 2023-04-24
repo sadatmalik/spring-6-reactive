8656d5a3521ff6239a45fabc3cffe04c255a33f4
sadat.malik <sadat.malik@me.com>
2023-04-24 09:39:00 +0100

    adding mapstruct dependency and type mapper

---
+++ src/main/java/com/creativefusion/spring6reactive/BeerMapper.java
@@ -0,0 +1,16 @@
+package com.creativefusion.spring6reactive;
+
+import com.creativefusion.spring6reactive.domain.Beer;
+import com.creativefusion.spring6reactive.model.BeerDTO;
+import org.mapstruct.Mapper;
+
+/**
+ * @author sm@creativefusion.net
+ */
+@Mapper
+public interface BeerMapper {
+    Beer beerDtoToBeer(BeerDTO dto);
+
+    BeerDTO beerToBeerDto(Beer beer);
+}
+
