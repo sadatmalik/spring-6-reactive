8fd35c6822a286b43705f47bc9c2d695fe97fa29
sadat.malik <sadat.malik@me.com>
2023-04-25 14:29:40 +0100

    adding unit tests for update validation failed

--- src/test/java/com/creativefusion/spring6reactive/controller/BeerControllerTest.java
+++ src/test/java/com/creativefusion/spring6reactive/controller/BeerControllerTest.java
@@ -74,6 +74,19 @@ class BeerControllerTest {
     }
 
     @Test
+    @Order(4)
+    void testUpdateBeerBadRequest() {
+        Beer testBeer = BeerRepositoryTest.getTestBeer();
+        testBeer.setBeerStyle("");
+
+        webTestClient.put()
+                .uri(BeerController.BEER_PATH_ID, 1)
+                .body(Mono.just(testBeer), BeerDTO.class)
+                .exchange()
+                .expectStatus().isBadRequest();
+    }
+
+    @Test
     @Order(999)
     void testDeleteBeer() {
         webTestClient.delete()
